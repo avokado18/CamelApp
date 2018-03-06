895a783fc0e10c3d5c6eeb3d5cec879c2ad8cce6
vproskuryakova <vproskuryakova@cinimex.ru>
2018-03-06 14:24:43 +0300

    can get message, transform to soap message and send(with 400 Bad Request)

--- src/main/java/ru/cinimex/camel_app/Route.java
+++ src/main/java/ru/cinimex/camel_app/Route.java
@@ -1,13 +1,31 @@
 package ru.cinimex.camel_app;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.spring.ws.SpringWebserviceConstants;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Route extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
-		from("{{route.from}}").end()
-				.to("{{route.to}}");
+		from("{{route.from}}")
+				.process(new StringToSoapProcessor())
+				.to("{{route.to}}").end();
+
+	}
+
+	private class StringToSoapProcessor implements Processor {
+		public void process(Exchange exchange) throws Exception
+		{
+			String xmlRequestForGoogleStockQuote = "<GetWeather xmlns=\"http://www.webserviceX.NET\"><CityName>Voronezh</CityName><CountryName>Russia</CountryName></GetWeather>";
+			String soapHeader = "<h:Header xmlns:h=\"http://www.webserviceX.NET/\"><h:MessageID>1234567890</h:MessageID><h:Nested><h:NestedID>1111</h:NestedID></h:Nested></h:Header>";
+			exchange.getOut().copyFrom(exchange.getIn());
+			exchange.getOut().setBody(xmlRequestForGoogleStockQuote);
+			exchange.getOut().setHeader(SpringWebserviceConstants.SPRING_WS_SOAP_HEADER, soapHeader);
+		}
 	}
 }
+
+
